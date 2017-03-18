4912fb06f81dd57956f0e3c26defc89d0eb9c579
Ernie Barber <erniebarber@me.com>
2017-03-17 22:51:06 -0400

    Add files via upload
    Compares two int[] arrays by checking indexes.

---
+++ ArrayCompare.java
@@ -0,0 +1,46 @@
+/*This program compares the indexes of two arrays
+for equality.
+Ernie Barber 
+CS142
+Spring 2017
+*/
+public class ArrayCompare
+{
+   public static void main(String[] args) 
+   {
+   //int []array1= {1,2,3,4,5};//test array
+   //int []array2= {1,2,3,4,5};//test array
+   int []array1 = {1,2,3,4,8};
+   int []array2 = {1,2,3,4,5,6,7};
+
+   boolean arrayEqual = true;
+   
+   if (array1.length != array2.length)
+      {
+         arrayEqual = false;
+      }   
+   
+   if (compare(array1, array2))
+      {
+         System.out.println("The two arrays are the same");
+      }
+   else
+      {
+         System.out.println("The two arrays are not the same");
+      }
+   
+   }
+   
+   public static boolean compare(int[]array1,int[]array2)
+   {
+      boolean arrayEqual= true;
+      for (int i = 0; i < array1.length; i++)
+         {
+            if (array1[i] != array2[i])
+               {
+               arrayEqual = false;
+               }
+         }
+   return arrayEqual;
+   }
+ }  
